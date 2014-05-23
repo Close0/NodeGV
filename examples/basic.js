e90daf37ad100b83bd26305ce953af6de0e3d05e
Sean Huitger <sean@codebysean.com>
2014-05-22 21:56:43 -0500

    Adding the shared self signed certs -- Also fixing the bug where the application would crash if voice data came in

--- examples/basic.js
+++ examples/basic.js
@@ -5,16 +5,15 @@ var mumble = require('../');
 var fs = require('fs');
 
 var options = {
-    key: fs.readFileSync( 'private.pem' ),
-    cert: fs.readFileSync( 'public.pem' )
+    key: fs.readFileSync( '../certs/node.key' ),
+    cert: fs.readFileSync( '../certs/node.crt' )
 }
 
 console.log( 'Connecting' );
-mumble.connect( 'mumble://example.org', options, function ( error, connection ) {
+mumble.connect( 'mumble://dogs.mumble.com:3560', options, function ( error, connection ) {
     if( error ) { throw new Error( error ); }
 
     console.log( 'Connected' );
-
     connection.authenticate( 'ExampleUser' );
     connection.on( 'initialized', onInit );
     connection.on( 'voice', onVoice );
@@ -22,12 +21,10 @@ mumble.connect( 'mumble://example.org', options, function ( error, connection ) 
 
 var onInit = function() {
     console.log( 'Connection initialized' );
-
     // Connection is authenticated and usable.
 };
 
-var onVoice = function( event ) {
+var onVoice = function( voice ) {
     console.log( 'Mixed voice' );
-
     var pcmData = voice.data;
 }
