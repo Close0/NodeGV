08d62a6cb01f6cae96f88fbd010068b0bc6126a6
Sean Huitger <Close0@gmail.com>
2014-07-11 11:36:04 -0400

    Join with lots of bots now!

--- examples/gamevox.js
+++ examples/gamevox.js
@@ -1,5 +1,56 @@
-var mumble = require('../');
 var fs = require('fs');
+var tls = require('tls');
+var MumbleConnection = require('../lib/MumbleConnection');
+
+var options = {
+	key: fs.readFileSync( '../certs/node.key' ),
+	cert: fs.readFileSync( '../certs/node.crt' ),
+	rejectUnauthorized: false
+};
+
+
+var userIndex = 1;
+
+var id = setInterval(function() {
+	if(userIndex > 5) { clearInterval(id); return; }
+	var socket = tls.connect( 32051, '50.22.63.228', options, function ( err ) {
+		// TODO Remove the certificate buffers from the options.
+		// MumbleConnection doesn't need to hold onto them.
+		var connection = new MumbleConnection( socket, options );
+		console.log( 'Connected' );
+		connection.authenticate( 'Username'+userIndex++,'gamevox' );
+		connection.on( 'authenticated', function(message) {
+			if(message.success == "1") {
+				connection.activate(1);
+			} else  {
+				console.log("We were not able to authenticate.");
+			}
+		});
+		connection.on( 'activated', function() {
+			connection.joinServer('rJRB3Ce73kvr');
+		});
+		connection.on( 'connectedToVS', function(message) {
+			console.log("We're connected to to the voice server: " + message.serverName);
+			connection.serverSync();
+		});
+		connection.on( 'serverSync', function(message) {
+			connection.generateSound();
+			setInterval(function() {
+				connection.disperse();
+				setTimeout(function() {
+					connection.assemble();
+				},2000);
+			},5000);
+		});
+	});
+},1000);
+
+
+
+
+/*
+var mumble = require('../');
+
 
 var options = {
 	key: fs.readFileSync( '../certs/node.key' ),
@@ -46,3 +97,4 @@ var onVoice = function( voice ) {
 	console.log( 'Mixed voice' );
 	var pcmData = voice.data;
 };
+*/
